2e54c0975cf039d2accffee7edb8e822a1902ace
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-08-22 14:16:53 +0530

    blog-website

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express"
-
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+import connectDB from "./lib/connectDB.js";
 const app = express()
 
+// app.get("/test",(req,res)=>{
+//   res.status(200).send("it orks!!")
+// })
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+
+
 app.listen(3000,()=>{
+  connectDB()
   console.log("Server is running")
 })
--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("MongoDB is connected");
-  } catch (err) {
-    console.log(err);
+    console.log("Connecting to MongoDB...");
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected ✅");
+  } catch (error) {
+    console.error("MongoDB connection error ❌", error);
+    process.exit(1); // stop server if DB fails
   }
 };
 
