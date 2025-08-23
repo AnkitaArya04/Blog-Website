66dcba16fea700e96c720c8c270cd97f5d5f59b2
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-08-23 23:38:13 +0530

    blog-website

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false, // prevent looking for .map files
+  },
+  css: {
+    devSourcemap: false, // disable sourcemaps for CSS
+  },
 })
+
